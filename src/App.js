222158a466b1f16d7c38a8310dbb76c099b7e300
stephengallo1977 <stephengallo1977@gmail.com>
2019-08-26 08:54:51 -0400

    addedCss

--- src/App.js
+++ src/App.js
@@ -1,59 +1,126 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import MatchCard from "./components/MatchCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import matches from "./matchcards.json";
 import "./App.css";
 
-// let data = [
-//   {
-//     name: "Ben",
-//     pets: ["Lana", "Bones"]
-//   },
-//   {
-//     name: "Laura",
-//     pets: ["Scrawny", "Hallie", "Sparkles"]
-//   }
-// ];
+let correctGuesses = 0;
+let bestScore = 0;
+let clickMessage =
+  "Click on an image to earn points, but don't click on any of them more than once!";
 
-// class App extends React.Component {
-//   state = { data }
+class App extends Component {
+  // Setting this.state.matches to the matches json array
+  state = {
+    matches,
+    correctGuesses,
+    bestScore,
+    clickMessage
+  };
 
-//   newPerson = (person) => {
-//     console.log(person)
-//     this.setState({
-//       data: this.state.data.push(person)
-//     })
-//   }
+  setClicked = id => {
+    // Make a copy of the state matches array to work with
+    const matches = this.state.matches;
 
-//   render() {
-//     return (
-//       <div className="App">
-//         {this.state.data.map(person => (
-//           <DispPerson person={person} />
-//         ))}
-//         <NewPerson newPerson={this.newPerson} />
-//       </div>
-//     );
-//   }
-// }
+    // Filter for the clicked match
+    const clickedMatch = matches.filter(match => match.id === id);
 
-// function NewPerson(props){
-//   return <input type="button" onClick={()=>props.newPerson({name: "Bob", pets: []})} value="Submit"/>
-// }
+    // If the matched image's clicked value is already true,
+    // do the game over actions
+    if (clickedMatch[0].clicked) {
+      console.log("Correct Guesses: " + correctGuesses);
+      console.log("Best Score: " + bestScore);
 
-// class DispPerson extends React.Component {
-//   render() {
-//     return (
-//       <>
-//         <DispName name={this.props.person.name} />
-//         {this.props.person.pets.map(pet => (
-//           <h5>{pet}</h5>
-//         ))}
-//       </>
-//     );
-//   }
-// }
+      correctGuesses = 0;
+      clickMessage =
+        "Dang! You already clicked on that one! Now you have to start over!";
 
-// function DispName(props) {
-//   return <h1>{props.name}</h1>;
-// }
+      for (let i = 0; i < matches.length; i++) {
+        matches[i].clicked = false;
+      }
 
-// export default App;
+      this.setState({ clickMessage });
+      this.setState({ correctGuesses });
+      this.setState({ matches });
+
+      // Otherwise, if clicked = false, and the user hasn't finished
+    } else if (correctGuesses < 11) {
+      // Set its value to true
+      clickedMatch[0].clicked = true;
+
+      // increment the appropriate counter
+      correctGuesses++;
+
+      clickMessage = "Great! You haven't click on that one yet! Keep going!";
+
+      if (correctGuesses > bestScore) {
+        bestScore = correctGuesses;
+        this.setState({ bestScore });
+      }
+
+      // Shuffle the array to be rendered in a random order
+      matches.sort(function(a, b) {
+        return 0.5 - Math.random();
+      });
+
+      // Set this.state.matches equal to the new matches array
+      this.setState({ matches });
+      this.setState({ correctGuesses });
+      this.setState({ clickMessage });
+    } else {
+      // Set its value to true
+      clickedMatch[0].clicked = true;
+
+      // restart the guess counter
+      correctGuesses = 0;
+
+      // Egg on the user to play again
+      clickMessage =
+        "WOW!!! You got ALL of them!!! Now, let's see if you can do it again!";
+      bestScore = 12;
+      this.setState({ bestScore });
+
+      for (let i = 0; i < matches.length; i++) {
+        matches[i].clicked = false;
+      }
+
+      // Shuffle the array to be rendered in a random order
+      matches.sort(function(a, b) {
+        return 0.5 - Math.random();
+      });
+
+      // Set this.state.matches equal to the new matches array
+      this.setState({ matches });
+      this.setState({ correctGuesses });
+      this.setState({ clickMessage });
+    }
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>To boldly click where no one has clicked before!</Title>
+
+        <h3 className="scoreSummary">{this.state.clickMessage}</h3>
+
+        <h3 className="scoreSummary">
+          Correct Guesses: {this.state.correctGuesses}
+          <br />
+          Best Score: {this.state.bestScore}
+        </h3>
+
+        {this.state.matches.map(match => (
+          <MatchCard
+            setClicked={this.setClicked}
+            id={match.id}
+            key={match.id}
+            image={match.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
